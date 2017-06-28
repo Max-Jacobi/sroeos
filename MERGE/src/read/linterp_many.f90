ebba7f5f2910220be390fd17e8da32d3a9c0b712
Christian D. Ott <cott@tapir.caltech.edu>
2017-06-27 17:46:40 -0700

    MERGE: fix another STOP issue

--- MERGE/src/read/linterp_many.f90
+++ MERGE/src/read/linterp_many.f90
@@ -62,7 +62,7 @@ SUBROUTINE intp3d_many ( x, y, z, f, kt, ft, nx, ny, nz, nvars, xt, yt, zt)
       real*8 dx,dy,dz,dxi,dyi,dzi,dxyi,dxzi,dyzi,dxyzi
       integer n,ix,iy,iz
 
-      IF (kt .GT. ktx)  STOP'***KTX**'
+      IF (kt .GT. ktx)  STOP '***KTX**'
 !
 !
 !------  determine spacing parameters of (equidistant!!!) table
